098074be9565705bf4ea5b301e335b5c9cd908f8
Ian McGreer <imcgreer@gmail.com>
2017-05-04 09:35:30 -0700

    split into base class with kcorr and selfun

--- simqso/sqanalysis.py
+++ simqso/sqanalysis.py
@@ -8,78 +8,6 @@ from scipy.stats import binned_statistic_2d
 from . import sqgrids as grids
 from .sqrun import readSimulationData
 
-class SimKCorr(object):
-	def __init__(self,qsoGrid,minNobj=10):
-		self.qsoGrid = qsoGrid
-		self.minNobj = minNobj
-		#if not isinstance(qsoGrid,grids.QsoSimGrid):
-		#	raise NotImplementedError
-		if len(qsoGrid.gridShape) > 3:
-			raise NotImplementedError(">2D k-corrections not supported yet")
-		if qsoGrid.units == 'flux':
-			self.mBins = qsoGrid.gridCenters[qsoGrid.varIndex('appMag')]
-			self.gridBand = qsoGrid.getVars('appMag').obsBand
-		else:
-			self.mBins = qsoGrid.gridCenters[qsoGrid.varIndex('absMag')]
-			self.gridBand = qsoGrid.getVars('absMag').restWave
-		#
-		self.zDimIndex = qsoGrid.varIndex('z')
-		self.zBins = qsoGrid.gridCenters[self.zDimIndex]
-		jj = [ np.s_[0] for i in range(qsoGrid.nGridDim) ]
-		jj[self.zDimIndex] = np.s_[:]
-		self.zEdges = qsoGrid.gridEdges[self.zDimIndex][jj]
-		#
-		self.DM = qsoGrid.distMod(qsoGrid.asGrid('z'))
-		self.appMag = qsoGrid.asGrid('synMag')
-		self.absMag = qsoGrid.asGrid('absMag')
-	def _from_grid(self):
-		kg = np.median(self.kCorrVals,axis=-1)
-		kfun = RectBivariateSpline(self.mBins,self.zBins,kg,kx=3,ky=3,s=1)
-		return dict(mBins=self.mBins,zBins=self.zBins,
-		            kCorrGrid=kg,kCorrFun=kfun)
-	def _resample_grid(self,mag,kCorrVals):
-		mag = mag.ravel()
-		kv = kCorrVals.ravel()
-		#
-		mStep = np.median(np.diff(self.mBins))
-		mLow = mag.min() - mStep
-		mHigh = mag.max() + mStep
-		mEdges = np.arange(mLow,mHigh,mStep)
-		#
-		zEdges = self.zEdges
-		z = np.array(self.qsoGrid.z)
-		#
-		kCorrGrid,_,_,ii = binned_statistic_2d(mag,z,kv,'median',
-		                                       [mEdges,zEdges])
-		n,_,_,_ = binned_statistic_2d(mag,z,kv,'count',[mEdges,zEdges])
-		kCorrGrid[n<self.minNobj] = np.nan
-		mBins = mEdges[:-1] + np.diff(mEdges)/2
-		zBins = zEdges[:-1] + np.diff(zEdges)/2
-		mm,zz = np.meshgrid(mBins,zBins,indexing='ij')
-		ii = np.where(np.isfinite(kCorrGrid))
-		kfun = SmoothBivariateSpline(mm[ii],zz[ii],kCorrGrid[ii],
-		                             kx=3,ky=3,s=1)
-		return dict(mBins=mBins,zBins=zBins,
-		            kCorrGrid=kCorrGrid,kCorrFun=kfun)
-	def calc_grid(self,band):
-		# extract the apparent mag array for the input band
-		b_j = self.qsoGrid.getBandIndex(band)
-		appMag = self.appMag[...,b_j]
-		# calculate k-correction by subtracting off the distance modulus
-		self.kCorrVals = (appMag - self.DM) - self.absMag
-		#
-		if self.qsoGrid.units == 'luminosity':
-			self.k_M = self._from_grid()
-			self.k_m = self._resample_grid(appMag,self.kCorrVals)
-		else:
-			self.k_m = self._from_grid()
-			self.k_M = self._resample_grid(self.absMag,self.kCorrVals)
-	def __call__(self,m,z,inverse=False):
-		if inverse:
-			return self.k_M['kCorrFun'].ev(m,z)
-		else:
-			return self.k_m['kCorrFun'].ev(m,z)
-
 class ClippedFunction(object):
 	def __init__(self,fun,minval=0.0,maxval=1.0):
 		self.fun = fun
@@ -92,13 +20,120 @@ class ClippedFunction(object):
 	def ev(self,*args,**kwargs):
 		return self.clip(self.fun.ev(*args,**kwargs))
 
-def calcSelectionFunctionFromGrid(fileName,selector,outputDir='./'):
-	simData,simPars = readSimulationData(fileName,outputDir,retParams=True)
-	mBins,zBins,gridShape = getGridBins(simPars)
-	is_selected = selector(simData['obsMag'],simData['obsMagErr'],
-	                       simData['obsFlux'],simData['obsFluxErr'])
-	is_selected = is_selected.reshape(gridShape)
-	selGrid = np.sum(is_selected,axis=-1).astype(np.float32) / gridShape[-1]
-	selFun = RectBivariateSpline(mBins,zBins,selGrid,kx=3,ky=3,s=1)
-	return ClippedFunction(selFun)
+class SimGridOutput(object):
+	def __init__(self,qsoGrid,minNobj=10):
+		self.qsoGrid = qsoGrid
+		self.minNobj = minNobj
+		#if not isinstance(qsoGrid,grids.QsoSimGrid):
+		#	raise NotImplementedError
+		if len(qsoGrid.gridShape) > 3:
+			raise NotImplementedError(">2D grids not supported yet")
+		if qsoGrid.units == 'flux':
+			self.mBins = qsoGrid.gridCenters[qsoGrid.varIndex('appMag')]
+			self.gridBand = qsoGrid.getVars('appMag').obsBand
+		else:
+			self.mBins = qsoGrid.gridCenters[qsoGrid.varIndex('absMag')]
+			self.gridBand = qsoGrid.getVars('absMag').restWave
+		#
+		self.zDimIndex = qsoGrid.varIndex('z')
+		self.zBins = qsoGrid.gridCenters[self.zDimIndex]
+		jj = [ np.s_[0] for i in range(qsoGrid.nGridDim) ]
+		jj[self.zDimIndex] = np.s_[:]
+		self.zEdges = qsoGrid.gridEdges[self.zDimIndex][jj]
+		self.absMag = self.qsoGrid.asGrid('absMag')
+	def _from_grid(self):
+		g = self._collapse_grid()
+		f = RectBivariateSpline(self.mBins,self.zBins,g,kx=3,ky=3,s=1)
+		return dict(mBins=self.mBins,zBins=self.zBins,grid=g,interp=f)
+	def _resample_grid(self,mag,vals):
+		mag = mag.ravel()
+		vals = vals.ravel()
+		#
+		mStep = np.median(np.diff(self.mBins))
+		mLow = mag.min() - mStep
+		mHigh = mag.max() + mStep
+		mEdges = np.arange(mLow,mHigh,mStep)
+		#
+		zEdges = self.zEdges
+		z = np.array(self.qsoGrid.z)
+		#
+		g = self._grid_binstats(mag,z,vals,[mEdges,zEdges])
+		mBins = mEdges[:-1] + np.diff(mEdges)/2
+		zBins = zEdges[:-1] + np.diff(zEdges)/2
+		mm,zz = np.meshgrid(mBins,zBins,indexing='ij')
+		ii = np.where(np.isfinite(g))
+		f = SmoothBivariateSpline(mm[ii],zz[ii],g[ii],kx=3,ky=3,s=1)
+		return dict(mBins=mBins,zBins=zBins,grid=g,interp=f)
+	def calc_grid(self,*args,**kwargs):
+		self._calc_vals(*args,**kwargs)
+		#
+		if self.qsoGrid.units == 'luminosity':
+			self.f_M = self._from_grid()
+			self.f_m = self._resample_grid(appMag,self.gridVals)
+		else:
+			self.f_m = self._from_grid()
+			self.f_M = self._resample_grid(self.absMag,self.gridVals)
+	def __call__(self,m,z,inverse=False):
+		if inverse:
+			return self.f_M['interp'].ev(m,z)
+		else:
+			return self.f_m['interp'].ev(m,z)
+
+class SimKCorr(SimGridOutput):
+	def __init__(self,qsoGrid,**kwargs):
+		super(SimKCorr,self).__init__(qsoGrid,**kwargs)
+		self.DM = self.qsoGrid.distMod(self.qsoGrid.asGrid('z'))
+		self.appMag = self.qsoGrid.asGrid('synMag')
+	def _calc_vals(self,band):
+		# extract the apparent mag array for the input band
+		b_j = self.qsoGrid.getBandIndex(band)
+		appMag = self.appMag[...,b_j]
+		# calculate k-correction by subtracting off the distance modulus
+		self.gridVals = (appMag - self.DM) - self.absMag
+	def _collapse_grid(self):
+		return np.median(self.gridVals,axis=-1)
+	def _grid_binstats(self,mag,z,vals,bins):
+		g = binned_statistic_2d(mag,z,vals,'median',bins)[0]
+		n = binned_statistic_2d(mag,z,vals,'count',bins)[0]
+		g[n<self.minNobj] = np.nan
+		return g
+
+# XXX add clippedfun back in
+class SelectionFunction(SimGridOutput):
+	def __init__(self,qsoGrid,m2M,**kwargs):
+		super(SelectionFunction,self).__init__(qsoGrid,**kwargs)
+		self.m2M = m2M
+		self.nPerBin = self.qsoGrid.gridShape[-1]
+	def _calc_vals(self):
+		s = self.selector(self.qsoGrid.obsMag,self.qsoGrid.obsMagErr,
+		                  self.qsoGrid.obsFlux,self.qsoGrid.obsFluxErr,
+		                  self.qsoGrid.photoBands)
+		self.gridVals = s.reshape(self.qsoGrid.gridShape)
+	def _collapse_grid(self):
+		return np.sum(self.gridVals,axis=-1) / float(self.nPerBin)
+	def _grid_binstats(self,mag,z,vals,bins):
+		g = binned_statistic_2d(mag,z,vals,'sum',bins)[0]
+		n = binned_statistic_2d(mag,z,vals,'count',bins)[0]
+		g[n<self.minNobj] = np.nan
+		return g/n
+	def _photo_complete_call(self,m,z,absMag=False):
+		if type(self.photo_complete) is float:
+			return self.photo_complete
+		elif absMag:
+			return self.photo_complete(m+self.m2M(m,z,inverse=True),z)
+		else:
+			return self.photo_complete(m,z)
+	def _spec_complete_call(self,m,z,absMag=False):
+		if type(self.spec_complete) is float:
+			return self.spec_complete
+		elif absMag:
+			return self.spec_complete(m+self.m2M(m,z,inverse=True),z)
+		else:
+			return self.spec_complete(m,z)
+	def _color_complete_call(self,m,z,absMag=False):
+		return super(SelectionFunction,self).__call__(m,z,absMag)
+	def __call__(self,m,z,absMag=False):
+		return self._color_complete_call(m,z,absMag) * \
+		       self._photo_complete_call(m,z,absMag) * \
+		       self._spec_complete_call(m,z,absMag)
 
