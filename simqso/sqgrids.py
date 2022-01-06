7268c724e9fd224f23c5bca0afc864fe534ac355
John Moustakas <john.moustakas@gmail.com>
2022-01-06 15:25:42 -0500

    handle missing blackbody_lambda astropy function

--- simqso/sqgrids.py
+++ simqso/sqgrids.py
@@ -18,7 +18,33 @@ from astropy.constants import sigma_sb,b_wien
 try:
     from astropy.modeling.blackbody import blackbody_lambda
 except ImportError:
-    from astropy.analytic_functions import blackbody_lambda
+    #from astropy.analytic_functions import blackbody_lambda
+    def blackbody_lambda(in_x, temperature):
+        import astropy.constants as const
+        import astropy.units as u
+        from astropy.utils.exceptions import AstropyUserWarning
+        # Units
+        FNU = u.erg / (u.cm**2 * u.s * u.Hz)
+        FLAM = u.erg / (u.cm**2 * u.s * u.AA)
+        def blackbody_nu(in_x, temperature):
+            # Convert to units for calculations, also force double precision
+            with u.add_enabled_equivalencies(u.spectral() + u.temperature()):
+                freq = u.Quantity(in_x, u.Hz, dtype=np.float64)
+                temp = u.Quantity(temperature, u.K, dtype=np.float64)
+            # Check if input values are physically possible
+            if temp < 0:
+                raise ValueError('Invalid temperature {0}'.format(temp))
+            if np.any(freq <= 0):  # pragma: no cover
+                warnings.warn('Input contains invalid wavelength/frequency value(s)',
+                              AstropyUserWarning)
+            # Calculate blackbody flux
+            bb_nu = (2.0 * const.h * freq ** 3 /
+                     (const.c ** 2 * np.expm1(const.h * freq / (const.k_B * temp))))
+            flux = bb_nu.to(FNU, u.spectral_density(freq))
+            return flux / u.sr  # Add per steradian to output flux unit
+        bb_nu = blackbody_nu(in_x, temperature) * u.sr  # Remove sr for conversion
+        flux = bb_nu.to(FLAM, u.spectral_density(in_x))
+        return flux / u.sr  # Add per steradian to output flux unit
 
 from .sqbase import datadir,Spectrum
 from . import dustextinction
